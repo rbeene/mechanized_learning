a4ff77d692372c4684a8c01e185e87a6f27f1009
Robert Beene <rbeene@teladoc.com>
2013-02-16 15:41:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MechanizeLearning::Application.config.secret_token = '3a880731e1fa82254c565f0cceb7e626be8e12b2b897117eaf461d7765e916048b65fb78b9f7c273d320412ca7c0d0a8e96daa64694fd3a13c4a4bdb3823bfe9'
